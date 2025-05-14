699cad23aab11ef76df0300809a63f122f0b938b
Carlos2537431 <carlosju129@gmail.com>
2025-05-14 10:10:35 -0300

    base

--- dashboard-usuarios/src/App.jsx
+++ dashboard-usuarios/src/App.jsx
@@ -6,15 +6,16 @@ function App() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost3001/peoples")
-      .then((response) => response.json())
+    fetch("http://localhost:3001/peoples")
+      .then((res) => res.json())
       .then((data) => setUsers(data.users))
-      .catch((error) => console.error("Error fetching users:", error));
+      .catch((err) => console.error("Error ao  buscar usuarios:", err));
   }, []);
+
   return (
     <div className="App">
       <h1>Dashboard de Usuarios</h1>
-      <p>Total de Usuarios: {users.lenght}</p>
+      <p>Total de Usuarios: {users.length}</p>
       <div className="user-container">
         {users.map((user) => (
           <UserCard key={user.id} user={user} />
